55bef36da000217df97d279d4257a461a01130e5
hyrrokkin <menshchikov.danil@mail.ru>
2020-04-27 19:06:32 +0300

    Role

--- app/src/entity/user.py
+++ app/src/entity/user.py
@@ -8,11 +8,14 @@ from app.src.entity import login as login_form
 
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(64), unique=True)
     name = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(120), unique=True)
     password_hash = db.Column(db.String(128))
+    role = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    role_object = db.relationship('Role', back_populates='users', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
